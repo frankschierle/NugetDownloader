9d96152f332b6a809cb5ba4e4bc78666f9a7ae95
Frank S. Schierle <git@frankschierle.de>
2017-05-25 15:06:05 +0200

    Package downloader implemented.
    Can be used to download a single NuGet package from the official NuGet repository (nuget.org).

---
+++ NugetDownloader.Core/IPackageDownloader.cs
@@ -0,0 +1,20 @@
+﻿namespace NugetDownloader.Core
+{
+  using System;
+  using System.IO;
+
+  /// <summary>Provides methods to download NuGet packages.</summary>
+  public interface IPackageDownloader
+  {
+    #region Public Methods
+
+    /// <summary>Downloads a Nuget package and saves it to the given target directory.</summary>
+    /// <param name="package">The package to download.</param>
+    /// <param name="targetFileName">The target file name to save the package to.</param>
+    /// <exception cref="ArgumentNullException">Occurs if any of the parameters is null.</exception>
+    /// <exception cref="IOException">Occurs if the <paramref name="targetFileName"/> already exist.</exception>
+    void DownloadPackage(PackageId package, string targetFileName);
+
+    #endregion
+  }
+}
---
+++ NugetDownloader.Core/PackageDownloader.cs
@@ -0,0 +1,100 @@
+﻿namespace NugetDownloader.Core
+{
+  using System;
+  using System.Diagnostics.CodeAnalysis;
+  using System.Globalization;
+  using System.IO;
+  using System.Net;
+  using System.Text;
+
+  /// <inheritdoc />
+  [SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Justification = "No unmanaged resources to dispose.")]
+  public class PackageDownloader : IPackageDownloader, IDisposable
+  {
+    #region Private Fields
+
+    /// <summary>The base download URL for NuGet packages.</summary>
+    private const string DownloadBaseUrl = "https://www.nuget.org/api/v2/package/";
+
+    /// <summary>The web client used to download packages.</summary>
+    private readonly WebClient webClient;
+
+    /// <summary>A flag inicating whether the object is disposed.</summary>
+    private bool disposed;
+
+    #endregion
+
+    #region Public Constructors
+
+    /// <summary>Initialises a new instance of the <see cref="PackageDownloader"/> class.</summary>
+    public PackageDownloader()
+    {
+      this.webClient = new WebClient();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <inheritdoc />
+    public void DownloadPackage(PackageId package, string targetFileName)
+    {
+      string downloadUrl;
+
+      if (package == null)
+      {
+        throw new ArgumentNullException(nameof(package));
+      }
+
+      if (targetFileName == null)
+      {
+        throw new ArgumentNullException(nameof(targetFileName));
+      }
+
+      if (this.disposed)
+      {
+        throw new ObjectDisposedException(this.GetType().Name);
+      }
+
+      if (File.Exists(targetFileName))
+      {
+        throw new IOException("Target file \"" + targetFileName + "\" already exists.");
+      }
+
+      downloadUrl = GetDownloadUrl(package);
+      this.webClient.DownloadFile(downloadUrl, targetFileName);
+    }
+
+    /// <inheritdoc />
+    [SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly", Justification = "No unmanaged resources to dispose.")]
+    public void Dispose()
+    {
+      this.webClient.Dispose();
+      this.disposed = true;
+      GC.SuppressFinalize(this);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>Gets the download URL for a given <see cref="PackageId"/>.</summary>
+    /// <param name="package">The package to get the download URL for.</param>
+    /// <returns>The download URL for the given package.</returns>
+    private static string GetDownloadUrl(PackageId package)
+    {
+      var urlBuilder = new StringBuilder(DownloadBaseUrl);
+
+      urlBuilder.Append(package.Name);
+
+      if (package.Version != null)
+      {
+        urlBuilder.AppendFormat(CultureInfo.InvariantCulture, "/{0}", package.Version);
+      }
+
+      return urlBuilder.ToString();
+    }
+
+    #endregion
+  }
+}
