7cd0fcd33a4b19b515ba490c622dbb06108fd986
Frank S. Schierle <git@frankschierle.de>
2017-05-26 17:30:05 +0200

    CLI project created.

---
+++ NugetDownloader.Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NugetDownloader.Cli
+{
+  /// <summary>Provides the application main entry point.</summary>
+  public static class Program
+  {
+    #region Public Methods
+
+    /// <summary>Application main entry point.</summary>
+    /// <param name="args">The command line arguments.</param>
+    public static void Main(string[] args)
+    {
+    }
+
+    #endregion
+  }
+}
